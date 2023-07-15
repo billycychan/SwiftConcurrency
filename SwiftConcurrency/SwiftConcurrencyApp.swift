3677e9cad3ae730fa03d8563b564d6b0807dc447
Billy Chan <billycychan.work@gmail.com>
2023-07-14 23:41:48 -0400

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by CHI YU CHAN on 14/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
